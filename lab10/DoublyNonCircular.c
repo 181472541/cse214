9a4bcf5f2cab9779f819888a08ff93b803d856af
Md Rabiul Islam Shaon <65609660+181472541@users.noreply.github.com>
2020-07-19 21:00:16 +0600

    Update DoublyNonCircular.c

--- lab10/DoublyNonCircular.c
+++ lab10/DoublyNonCircular.c
@@ -1,45 +1,49 @@
+/* Design a Double way non circular linked list */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include "DoublyNonCircular.h"
 
-void display(head);
+// Function Prototype
+void display();
 node* push(node *head);
 node* pop(node *head);
 node* other_operations(node *head);
 
+// Main Function
 int main()
 {
     int op;
-    node *head = NULL;
-    head = create_list();
     do
     {
         system("cls");
-        printf("Select Your Option: \n");
+        printf("Select Your Choice: \n");
         printf("1. DISPLAY\n");
         printf("2. PUSH\n");
         printf("3. POP\n");
         printf("4. OTHER OPERATIONS\n") ;
         printf("5. Close\n");
-        printf("Enter Your Option Number: ");
+        printf("\nEnter Choice: ");
         scanf("%d", &op);
         if(op<=0 || op>5)
         {
-            printf("Invalid Option Number. Insert between 1 to 5\n");
+            printf("\t\t\t!!! Invalid Choice !!!\nPlease Insert Between 1 to 5\n");
+            getchar();
+            printf("\nPress Enter to Continue");
+            getchar();
 
         }
         else if(op == 1)
         {
-            printf("Linked List: ");
-            display(head);
+            display();
         }
         else if(op == 2)
         {
-            head = push(head);
+            push(head);
         }
         else if(op == 3)
         {
-            head = pop(head);
+            pop(head);
         }
         else if(op == 4)
         {
@@ -49,92 +53,106 @@ int main()
     while(op!=5);
 }
 
-void display(node *head)
+// Display Function
+void display()
 {
     display_list(head);
     getchar();
-    printf("Press Enter to Continue");
+    printf("\n\nPress Enter to Continue");
     getchar();
 };
 
+// PUSH Function
 node* push(node *head)
 {
     int op, value;
     do
     {
         system("cls");
-        printf("Select Your Option: \n");
+        printf("Select Your Choice: \n");
         printf("1. Push Front\n");
         printf("2. Push Back\n");
         printf("3. Back to Main Menu\n");
-        printf("Enter Your Option Number: ");
+        printf("\nEnter Choice: ");
         scanf("%d", &op);
         if(op<=0 || op>3)
         {
-            printf("Invalid Option Number. Insert between 1 to 3\n");
+            printf("\t\t\t!!! Invalid Choice !!!\nPlease Insert Between 1 to 3\n");
+            getchar();
+            printf("\nPress Enter to Continue");
+            getchar();
 
         }
         else if(op<3)
         {
-            printf("Input Value to PUSH: ");
-            scanf("%d", &value);
             if(op==1)
             {
-                return push_front(head, value);
+                push_front();
             }
             else if(op==2)
             {
-                return push_back(head, value);
+                push_back();
             }
         }
     }
     while(op!=3);
     return head;
 };
+
+// POP Function
 node* pop(node *head)
 {
     int op, value;
     do
     {
         system("cls");
-        printf("Select Your Option: \n");
+        printf("Select Your Choice: \n");
         printf("1. Pop Front\n");
         printf("2. Pop Back\n");
         printf("3. Clear\n");
         printf("4. Back to Main Menu\n");
-        printf("Enter Your Option Number: ");
+        printf("\nEnter Choice: ");
         scanf("%d", &op);
         if(op<=0 || op>4)
         {
-            printf("Invalid Option Number. Insert between 1 to 4\n");
+            printf("\t\t\t!!! Invalid Choice !!!\nPlease Insert Between 1 to 4\n");
+            getchar();
+            printf("\nPress Enter to Continue");
+            getchar();
 
         }
         else if(op<4)
         {
             if(op==1)
             {
-                return pop_front(head);
+                pop_front();
             }
             else if(op==2)
             {
-                return pop_back(head);
+                pop_back();
             }
             else if(op==3)
             {
-                return clear(head);
+                clear(head);
+                printf("\t\t>>> Delete All Node From List <<<");
+                getchar();
+                printf("\n\nPress Enter to Continue");
+                getchar();
             }
         }
     }
     while(op!=4);
     return head;
 };
+
+// Others Function
 node* other_operations(node *head)
 {
     int op;
     do
     {
         system("cls");
-        printf("Select Your Option: \n");
+        printf("Select Your Choice: \n");
         printf("1. Head\n");
         printf("2. Tail\n");
         printf("3. Empty\n");
@@ -142,57 +160,61 @@ node* other_operations(node *head)
         printf("5. Front\n") ;
         printf("6. Back\n") ;
         printf("7. Back to Main Menu\n");
-        printf("Enter Your Option Number: ");
+        printf("\nEnter Choice: ");
         scanf("%d", &op);
         if(op<=0 || op>7)
         {
-            printf("Invalid Option Number. Insert between 1 to 7\n");
+            printf("\t\t\t!!! Invalid Choice !!!\nPlease Insert Between 1 to 7\n");
+            getchar();
+            printf("\nPress Enter to Continue");
+            getchar();
         }
         else if(op<7)
         {
             if(op==1)
             {
-                printf("Head: %d\n", front(head));
+                printf("List Head: %d\n", front(head));
                 getchar();
-                printf("Press Enter to Continue");
+                printf("\nPress Enter to Continue");
                 getchar();
             }
             else if(op==2)
             {
-                printf("Tail: %d\n", back(head));
+                printf("List Tail: %d\n", back(head));
                 getchar();
-                printf("Press Enter to Continue");
+                printf("\nPress Enter to Continue");
                 getchar();
             }
             else if(op==3)
             {
                 empty(head);
                 getchar();
-                printf("Press Enter to Continue");
+                printf("\nPress Enter to Continue");
                 getchar();
             }
             else if(op==4)
             {
                 size(head);
                 getchar();
-                printf("Press Enter to Continue");
+                printf("\nPress Enter to Continue");
                 getchar();
             }
             else if(op==5)
             {
-                printf("Front: %d\n",front(head));
+                printf("List Front: %d\n",front(head));
                 getchar();
-                printf("Press Enter to Continue");
+                printf("\nPress Enter to Continue");
                 getchar();
             }
             else if(op==6)
             {
-                printf("Back: %d\n", back(head));
+                printf("List Back: %d\n", back(head));
                 getchar();
-                printf("Press Enter to Continue");
+                printf("\nPress Enter to Continue");
                 getchar();
             }
         }
     }
     while(op!=7);
+    return head;
 };
